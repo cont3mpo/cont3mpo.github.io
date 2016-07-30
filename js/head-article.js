c4f6aa3e28c64aafcaed50d7926dbc26a2eb801d
cont3mpo <ruben@Macbookcito.local>
2016-07-29 22:19:16 -0400

    Moviendo el blog a GitHub

---
+++ js/head-article.js
@@ -0,0 +1,12 @@
+document.write('\
+\
+  <meta name="description" content="Software, diseño, y Anime">\
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">\
+  <meta name="theme-color" content="#60666A">\
+  <meta name="apple-mobile-web-app-capable" content="yes">\
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">\
+  <link rel="shortcut icon" type="image/png" href="../../img/favicon.png">\
+  <link rel="stylesheet" href="../../css/normalize.css">\
+  <link rel="stylesheet" href="../../css/style.css">\
+\
+');
---
+++ js/header.js
@@ -0,0 +1,13 @@
+document.write('\
+\
+  <div class="logo">\
+    <a href="https://cont3mpo.github.io" title="Inicio">Contempo</a>\
+  </div>\
+  <nav>\
+    <ul>\
+      <li><a href="../../articulos.html" title="Artículos del blog">Artículos</a></li>\
+      <li><a href="../../yo.html" title="Sobre mi">Yo</a></li>\
+    </ul>\
+  </nav>\
+\
+');
