aeeb6a3bde75e54ad4c7f12ad63230d0d117bea7
cont3mpo <ruben@Macbookcito.local>
2016-09-17 18:23:17 -0300

    Edición de Probando HyperTerm, y modificado el head.

--- js/head.js
+++ js/head.js
@@ -1,6 +1,7 @@
 document.write('\
 \
-  <meta name="description" content="Software, diseño, y Anime">\
+  <meta name="description" content="Hablo de Software, diseño, y Anime">\
+  <meta name="author" content="Cont3mpo - twitter.com/cont3mpo">\
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">\
   <meta name="theme-color" content="#60666A">\
   <meta name="apple-mobile-web-app-capable" content="yes">\
