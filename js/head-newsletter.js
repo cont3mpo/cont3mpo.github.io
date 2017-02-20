b70b882cfd24e4768ccc570953b46c7ef71acaab
Rubén <cont3mpo@gmail.com>
2017-02-20 19:09:51 -0300

    Add files via upload

---
+++ js/head-newsletter.js
@@ -0,0 +1,13 @@
+document.write('\
+\
+  <meta name="description" content="Hablo de Software, diseño, y Anime">\
+  <meta name="author" content="Cont3mpo - twitter.com/cont3mpo">\
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">\
+  <meta name="theme-color" content="#60666A">\
+  <meta name="apple-mobile-web-app-capable" content="yes">\
+  <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">\
+  <link rel="shortcut icon" type="image/png" href="img/favicon-newsletter.png">\
+  <link rel="stylesheet" href="css/normalize.css">\
+  <link rel="stylesheet" href="css/style.css">\
+\
+');
