60fa4604e08306e3d8ae3f9a53717f04c4136aa6
Rubén <cont3mpo@gmail.com>
2017-07-31 19:34:58 -0400

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,13 +1,12 @@
 document.write('\
 \
   <div class="logo">\
-    <a href="https://cont3mpo.github.io" title="Inicio">Contempo</a>\
+    <a href="https://cont3mpo.github.io" title="Inicio">Contempo.</a>\
   </div>\
   <nav>\
     <ul>\
       <li><a href="../../blog.html" title="Blog">Blog</a></li>\
       <li><a href="../../articulos.html" title="Artículos del blog">Artículos</a></li>\
-      <li><a href="../../yo.html" title="Sobre mi">Yo</a></li>\
     </ul>\
   </nav>\
 \
