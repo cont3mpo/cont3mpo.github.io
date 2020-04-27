bfb32e6770a40996f70aa7316cf14b2ec9d6aa95
Rubén <cont3mpo@gmail.com>
2020-04-26 21:13:34 -0400

    Update head.js

--- js/head.js
+++ js/head.js
@@ -1,7 +1,8 @@
 document.write('\
 \
-  <meta name="description" content="Hablo de Software, diseño, y Anime">\
+  <meta name="description" content="Hablo de Software, diseño y Anime">\
   <meta name="author" content="Cont3mpo - twitter.com/cont3mpo">\
+  <meta http-equiv="Content-Security-Policy: default-src 'none'; script-src 'self'; img-src 'self'; style-src 'self'; connect-src 'self'">\
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">\
   <meta name="theme-color" content="#60666A">\
   <meta name="apple-mobile-web-app-capable" content="yes">\
