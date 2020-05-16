de566df5ad475535b2c31e02b611044ce80cbf15
Rubén <cont3mpo@gmail.com>
2020-05-16 14:55:12 -0400

    Update head-newsletter.js

--- js/head-newsletter.js
+++ js/head-newsletter.js
@@ -2,7 +2,7 @@ document.write('\
 \
   <meta name="description" content="Noticias semanales sobre Anime.">\
   <meta name="author" content="Rubén - twitter.com/cont3mpo">\
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">\
+  <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">\
   <link rel="shortcut icon" type="image/png" href="img/favicon-newsletter.png">\
   <link rel="stylesheet" href="css/normalize.css">\
   <link rel="stylesheet" href="css/style.css">\
