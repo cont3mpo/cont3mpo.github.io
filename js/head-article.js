3c0d8a1af5d8877b8f38b47f26e254fc6659f940
Rubén <cont3mpo@gmail.com>
2020-05-16 14:55:36 -0400

    Update head-article.js

--- js/head-article.js
+++ js/head-article.js
@@ -2,7 +2,7 @@ document.write('\
 \
   <meta name="description" content="Hablo de Software, diseño y Anime">\
   <meta name="author" content="Rubén - twitter.com/cont3mpo">\
-  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">\
+  <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">\
   <link rel="shortcut icon" type="image/png" href="../../img/favicon.png">\
   <link rel="stylesheet" href="../../css/normalize.css">\
   <link rel="stylesheet" href="../../css/style.css">\
