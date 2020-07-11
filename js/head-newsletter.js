9e6b8ad17000a262d216885a7ed09cc2d198f4a0
Rubén <cont3mpo@gmail.com>
2020-07-11 18:27:32 -0400

    Update head-newsletter.js

--- js/head-newsletter.js
+++ js/head-newsletter.js
@@ -4,7 +4,7 @@ document.write('\
   <meta name="author" content="Rubén - twitter.com/cont3mpo">\
   <meta name="viewport" content="width=device-width, shrink-to-fit=no, initial-scale=1">\
   <link rel="shortcut icon" type="image/png" href="img/favicon-newsletter.png">\
-  <link rel="stylesheet" href="css/normalize.css">\
-  <link rel="stylesheet" href="css/style.css">\
+  <link rel="preload stylesheet" href="css/normalize.css" as="style">\
+  <link rel="preload stylesheet" href="css/style.css" as="style">\
 \
 ');
